84f28e2a92dc8f6efbc03a9302139f246a386e06
moriss <marrocco001@gmail.com>
2021-01-19 07:01:41 +0000

    all

--- modulo.c
+++ modulo.c
@@ -5,19 +5,21 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <unistd.h>
-//Determine if a user's input is odd or even using modulo
+//Check if a user's input is an even integer using modulo
 
 int main (void)
 {
-  int user = get_int("Enter a number: ");
-  if( user % 2 == 0 )//If price  modulo 2 is zero
+  int user = get_int("Enter a number: ");//Get an integer from the user
+ 
+  if( user % 2 == 0 )//If price  modulo 2 is zero...
   {
-    printf("%i  is an even number " , user);
-    return 0;
+    printf("%i  is an even number " , user);//Confirm onscreen it is even
+    return 0;//Program ends here with even number selected
   }
-  printf("%i  is an odd number" , user );
-  return 1;
+  printf("%i  is an odd number" , user );//Confirm oncscreen it is odd
+  return 1;//programs 'falls through' to here if odd. 
 }
+
 //################################################################
 #include <cs50.h>
 #include <stdio.h>
@@ -26,8 +28,7 @@ int main (void)
 #include <stdlib.h>
 #include <ctype.h>
 #include <unistd.h>
-//Check if a user's input is an even integer using modulo
-//Then try with your own program
+//Check if a user's input is an even integer using modulo with your own function
 int my(int x);
 int main (void)
 {
@@ -45,9 +46,5 @@ int my(int x)
   printf("%i  is an odd number" , x );
   return 1;//programs 'falls through' to here if odd. 
 }
+
 //######################################################
-
-
-
-
-
