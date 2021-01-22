a8212dd7c5630f65dbfd4c82ef71191ad1f987f6
moriss <marrocco001@gmail.com>
2021-01-22 16:23:20 +0000

    operators

---
+++ operators_reference.c
@@ -0,0 +1,46 @@
+//operators
+
+//i ++  is same as i = i + 1
+//i --	is same as i = i - 1
+
+//x *= 5  is the same as x = x * 5 
+
+
+//boolean --> any positive numbers return true. only 0 returns false
+		  
+//##################################	  
+//logical '&&' operators
+// a && b
+//Both a and b need to be true
+
+//##################################
+
+//logical || 'or' operators 
+// a || b
+//requires only one to be true
+
+//##################################
+
+//The not operator 
+// a != b
+//'a' is not equal to 'b'
+//'!' reverses the state. It changes true to false and vice versa
+//Also refered to as bang and not
+//######################################
+
+
+
+//####################################
+//Relational operators
+
+// x < y    --> 'x' is less than 'y'
+
+// x > Y   -->  'x' is greater than 'y'
+
+// x <= y   --> 'x' is less than or equal to 'y'
+// x >= y   --> 'x' is greater than or equal to 'y'
+
+// x == y   : Equality. x is same as y
+// x != y   : Inequality x is not the same as y
+
+
