e73a930f007108e88d3a46dfeb778dd9602cc9fe
moriss <marrocco001@gmail.com>
2021-11-02 20:18:59 +0000

    Update credit_2.c

--- credit_2.c
+++ credit_2.c
@@ -1,22 +1,21 @@
-#include <cs50.h>
-#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+//NEXT = c_practice credit_2.c
 //Add up every digit of a credit card number
 int main(void)
 {
-   long credit;//Declare a long variable
-   credit = get_long("Card Number " );//get card number from user and put into 'credit' variable
-   int digit;//Used to pick out each digit at each loop rotation.
-   int adthem = 0;//For adding up the digits as you work through the loop
-
-   while (credit > 0)//Lets keep looping while credit is greater than zero
-   {
-      digit = credit % 10;//put the individual digit into 'digit'
-      adthem = adthem + digit;//Keep adding digit into adthem  until the loop stops
-      credit = credit / 10 ; //Reduce credit (divide by ten) to get to the next digit in sequence
-   }
-   printf("%i\n", adthem);//Tarra!
+    int x, addup = 0;//Create variables to store lastmost digit and add up all digits.
+    long number = get_long("Card Number: ");//Prompt user for their card number
+    while(number  != 0)//Loop through below while number hasn't yet reached zero.
+    {
+        x = number % 10;//put lastmost digit into x
+        addup = addup + x;//add x to the addup variable initially set at zero.
+        number = number / 10;//divide number by ten to reach the next digit to the left.
+    }
+    printf("%i \n" , addup);//Print total sum of addup
 }
 
 
-
-
+//Below have already been done:
+//c_practice credits_1
+//c_practice credits_2
