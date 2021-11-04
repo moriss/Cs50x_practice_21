26c0e14169617a19b44fbbc6723c35f51abc31fc
moriss <marrocco001@gmail.com>
2021-11-04 08:19:26 +0000

    Update credit_3.c

--- credit_3.c
+++ credit_3.c
@@ -1,28 +1,29 @@
-#include <cs50.h>
 #include <stdio.h>
-//Ask for user's credit card number
-//Add up every digit of the card number
-//Print out number of digits
-
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdlib.h>
+//NEXT = c_practice credit_3.c
+//Ask for user's credit card number then a) add up every digit and  b) print out number of digits.
+//Example: card number 2222. a)=8  b)=4
 int main(void)
 {
-   long credit;//Declare a long variable
-   credit = get_long("Card Number " );//get card number from user and put into 'credit' variable
-   int digit;//Used to pick out each digit at each loop rotation.
-   int adthem = 0;//For adding up the digits as you work through the loop
-   int digit_count = 0;
-
-   while (credit > 0)//Lets keep looping while credit is less than zero
-   {
-      digit = credit % 10;//put the individual digit into 'digit'
-      adthem = adthem + digit;//Keep adding digit into adthem  until the loop stops
-      digit_count = digit_count + 1;
-      credit = credit / 10 ; //Reduce credit (divide by ten) to get to the next digit in sequence
-   }
-   printf("Sum of Digits = %i  \n", adthem);//Tara!
-   printf("Number of Digits = %i  ", digit_count);//Tara!
+    long card_number = get_long("Card Number: ");//Prompt user for card number
+    long x = card_number;//Put card_number into x to preserve card_number for any future tasks.
+    int count_digits = 0, addup = 0, digit;//Set variables to count digits, add them up and 'digit' to isolate rightmost digit.
+    while(x != 0)//Set while loop condition to loop while card is above zero.
+    {
+        digit = x % 10;//Isolate rightmost digit putting it into x
+        addup = addup + digit;//Place digit in the addup 'accumulator', so digit is added at each cycle of the loop.
+        count_digits ++;//Increment count_digits by one to count number of digits.
+        x = x / 10;//put x divided by 10 into x to access next rightmost digit (done at first step in loop above)
+    }
+    printf("Sum= %i \nTotal Digits= %i \n", addup, count_digits);
 }
 
 
-
-
+//Below have already been done:
+//c_practice credits_1
+//c_practice credits_2
+//c_practice credit_3
