9d0436a311f752b5a8ebcb6963c54a0e6a78c0ea
moriss <marrocco001@gmail.com>
2021-11-21 21:10:34 +0000

    Update credit_6.c

--- credit_6.c
+++ credit_6.c
@@ -76,4 +76,80 @@ int main (void)
 //Visa numbers start with 4   and                       13- and 16-digits   VISA\n
 
 
+//BELOW IS ANOTHER WAY TO DO IT
 
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdlib.h>
+//NB TEST LUHNS ALOGRYTHM WITH YOUR CREDIT CARD TO WORK OUT WHAT'S WRONG.
+//c_practice credit_6
+//Do the complete cs50 credit card task in its entirety and test it.
+//https://cs50.harvard.edu/x/2020/psets/1/credit/#:~:text=34%20or%2037
+int main(void)
+{
+    int digit, digit_1, digit_2, addup  = 0, count = 0, times, greater;
+    long card = get_long("Card Number");
+    long x = card;
+    do
+    {
+        
+        digit = x % 10;
+            if(count % 2 != 0)//Oddly positioned numbers
+                {
+                    addup = addup + digit * 2;//Add digit times two to addup for oddly positioned numbers
+                    if (digit * 2 > 9)
+                    {
+                        addup = addup + ( digit % 10 + digit / 10);
+                    }
+                }
+                else {
+                    addup = addup + digit;//Add digit to addup for evenly positioned numbers
+                }
+                x = x / 10;//divide x by ten to get to next digit
+                count ++;//Increment count by one
+    }
+    while(x != 0);
+    
+    digit_1 = digit; digit_2 = digit + 1;//create varialbes for last and second last digits.
+
+    if( (addup % 10 == 0) && (digit_1 == 3)  &&  (digit_2 == 4 || digit_2 == 7) )//**If luhn is true, and digits are correct...
+    {
+        printf("AMEX");//print this fo amex
+    }
+    else if(addup % 10 == 0 && digit_1 == 4)//**
+    {
+        printf("VISA");//print this for visa
+    }
+    else if((addup % 10 == 0) && (digit_1 == 5) && (digit_2 >= 0 && digit_2 >= 6 ))//**
+    {
+        printf("MASTERCARD");//print this for mastercard
+    }
+    else
+    {
+        printf("%i INVALID\n", addup);//If not true or numbers don't match up, print invalid
+    }
+}
+
+//I tested this here https://developer.paypal.com/docs/payflow/payflow-pro/payflow-pro-testing/  Some worked and some did'nt.
+//I also "short" tested it by making up numbers that fitted each card and where addup returned zero. These all worked for each card. 
+//Amex start 34 or 37
+//Mastercard start 51, 52, 53, 54, or 55
+//Visa start with No 4
+//CHECKSUM:
+//Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together.
+//Add the sum to the sum of the digits that weren’t multiplied by 2.
+//If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!
+//Output AMEX\n or MASTERCARD\n or VISA\n or INVALID\n
+
+
+//Purpose of Luhn's algorythm = Detect typos without having to refer to a database
+
+
+//Below have already been done:
+//c_practice credits_1
+//c_practice credits_2
+//c_practice credit_3
+//c_practice credit_4
