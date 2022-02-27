ac903886212f3e26b73dcab23a008e4dfb939e28
moriss <marrocco001@gmail.com>
2022-02-27 21:36:29 +0000

    Update strings.c

--- strings.c
+++ strings.c
@@ -1,1 +1,11 @@
 
+/*There is no such thing as a string.
+  String is really just char *.
+  
+  char * x is a pointer, pointing to the address of the first byte of the string. 
+  The null terminating character \0, indicates the end of the string.
+  
+  char *x effectively removes the cs50 training wheels (previously string) so the cs50 library is no longer required. 
+  
+  */
+
