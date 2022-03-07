c5920b4a4b54a0beed28f0e31ed9ca8ef1708f33
moriss <marrocco001@gmail.com>
2022-03-07 23:10:15 +0000

    Update pointers.c

--- pointers.c
+++ pointers.c
@@ -20,5 +20,29 @@ int main(void)
     printf("%i \n", *b);//de-reference the pointer variable
 }
 
+//More detail of what you can ad can't do (below)
+int main(void)
+{
+
+   char *y = "Emma";
+
+   printf("%p \n", &y);//Address where y is located
+   printf("%p \n", y);//Value of y.(Address of first character)
+   printf("%c \n", *y);//This goes to address (stored in y) and prints the value at that address [1st letter 'E']
+   
+   printf("%c \n", y[1]);//This works for accessing value at second position.
+   printf("%c \n", y+1);//!!ACCESSING VALUE WITH +1 DOES NOT WORK IN THIS WAY. SEE NEXT ONE DOWN FOR CORRECTION
+   printf("%c \n", *y+1);//3. YOU NEED TO USE * TO 'GO TO' y, TO THEN ACCESS WHAT IS AT THE SECOND BYTE.
+   printf("%p \n", y+1);//4.  This however works for accessing the address at the second byte.
+   printf("%p \n", y[1]);//!!ACCESSING ADDRESS WITH [1] DOES NOT WORK.
+   printf("%p \n", *y[1]);//!!THIS DOES NOT WORK EITHER.
+    
+    //Best way to remember abstractly is:
+    //With values AND  addresses, of first byte, both can be accessed with 
+    //With (only) values, * and + work together fine. 3.
+    //Without the '*', you must resort to only accessing the address with '+'. 4.
+    
+}
+
 
 
