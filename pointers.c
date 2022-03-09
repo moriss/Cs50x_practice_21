c914c864440b2aab2e24739fd712832fbef35ae6
moriss <marrocco001@gmail.com>
2022-03-09 18:31:02 +0000

    Update pointers.c

--- pointers.c
+++ pointers.c
@@ -25,39 +25,23 @@ int main(void)
 {
 
    char *y = "Emma";
-    
-   //moving through variable's addresses, moving through values in logical sequence(abc...) and moving through variable's chars.
-   
-    //moving through variable's chars.
+   //moving through addresses and moving through values
+   //Moving throught values
    printf("%c\n", y[0]);   printf("%c\n", y[1]);    printf("%c\n", y[2]);
 
-   //moving through variable's addresses
+   //Moving through addresses in sequence
    printf("%p\n", y); printf("%p\n", y+1); printf("%p\n", y+2);
 
    //Accessing the next value in logical sequence, NOT the value at the next address.
    //If value at zero is e, value at 1 is f  and so on...
-   printf("%c\n", *y+1);   printf("%c\n", *y+2);   printf("%c\n", *y+3);
-   
-   
+   printf("%c %c %c\n", *y+1 ,*y+2 , *y+3);
+   //This is differrent to bellow
+
    //Spot the difference! Using *brackets allows you to access the actual values in the string. MORE USEFUUL THAN ABOVE!
-   printf("%c\n", *(y+1));   printf("%c\n", *(y+2));   printf("%c\n", *(y+3));
-
-
-   printf("%p \n", &y);//Address where y is located
-   printf("%p \n", y);//Value of y.(Address of first character)
-   printf("%c \n", *y);//This goes to address (stored in y) and prints the value at that address [1st letter 'E']
-   
-   printf("%c \n", y[1]);//This works for accessing value at second position.
-   printf("%c \n", y+1);//!!ACCESSING VALUE WITH +1 DOES NOT WORK IN THIS WAY. SEE NEXT ONE DOWN FOR CORRECTION
-   printf("%c \n", *y+1);//3. YOU NEED TO USE * TO 'GO TO' y, TO THEN ACCESS WHAT IS AT THE SECOND BYTE.
-   printf("%p \n", y+1);//4.  This however works for accessing the address at the second byte.
-   printf("%p \n", y[1]);//!!ACCESSING ADDRESS WITH [1] DOES NOT WORK.
-   printf("%p \n", *y[1]);//!!THIS DOES NOT WORK EITHER.
-    
-    //Best way to remember abstractly is:
-    //With values AND  addresses, of first byte, both can be accessed with 
-    //With (only) values, * and + work together fine. 3.
-    //Without the '*', you must resort to only accessing the address with '+'. 4.
+   printf("%c %c %c\n", *(y+1), *(y+2), *(y+3));
+   //Even more important to note is that the use of the brackets is known as syntatic sugar.
+   //It is effectively replacing the use of square brackets as below
+   printf("%c %c %c\n " , y[0], y[1] ,y[2]);
     
 }
 
