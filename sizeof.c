91fe4c569c0fe5ae44a8ed21b8a60c364a27c812
moriss <marrocco001@gmail.com>
2022-03-26 07:16:56 +0000

    Update sizeof.c

--- sizeof.c
+++ sizeof.c
@@ -8,7 +8,7 @@
 void mali(void);
 //The sizeof excercise
 //This simply shows that when allocating space (10 ints as per below), you have allocated zero to 9 int spaces.
-//The contents of memory at 10 is random as it breaks through our space allocation.
+//The contents of memory at 10 is random as it breaks through our space allocation. Known as a buffer overflow.
 int main (void)
 {
   mali();
