68074e8fb04df363df9217d6aa071b4f9edc412a
moriss <marrocco001@gmail.com>
2022-03-29 22:59:39 +0100

    Create algorythms.c

---
+++ algorythms.c
@@ -0,0 +1,19 @@
+//11.03 in of week 3
+//https://cs50.harvard.edu/x/2020/weeks/3/
+//Linear Search: Search each element in an array one at a time in a line.  aka an alogorythm ie: searching for the number 50
+//for i from 0 to n-1
+  //if the i'th element is 50, return true;  (The  bool)
+
+//if not found: return false;
+
+
+
+//Bineary Search Divide and conquer aka binary search
+
+//if not in list
+ //return false
+ //if middle item is 50: return true
+ //elsif 50 < middle element
+ //Next, search left half
+ //elsif 50 > middle element
+ //search the right half
