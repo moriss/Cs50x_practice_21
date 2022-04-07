334f9f40390a44dd3e285278c9f483b1d1412fc6
moriss <marrocco001@gmail.com>
2022-04-07 22:57:44 +0100

    Update compare.c

--- compare.c
+++ compare.c
@@ -9,15 +9,13 @@ void mali(void);
 //Compare 2 strings to test if they are the same
 int main (void)
 {
-  char *names_1 =  get_string("Name 1:");
-  char *names_2 =  get_string("Name 1:");
-  if(strcmp(names_1 , names_2) == 0)
+  char *names_1 =  get_string("Name 1:");//input firt name
+  char *names_2 =  get_string("Name 1:");//input second name
+  if(strcmp(names_1 , names_2) == 0)//zero is  outputed if strings are the same
   {
     printf("Same");
-    return 0;
+    return 0;// Returning zero here, stops the program here if true. 
   }
   printf("Different");
-  return 1;
-
-
+  //If return wasn't used above, both 'same' and 'different' would show on screen.
 }
