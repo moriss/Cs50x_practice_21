9e6d040959ac6fcda8000e8580863cb87bd57f36
moriss <marrocco001@gmail.com>
2024-04-09 14:09:25 +0100

    all

---
+++ AAtest.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	
+	printf("Hello %s \n", get_string("WHat is your name " ));
+
+	//  Absolute file path
+	// /home/maurizio/Desktop/Cs50x_practice_21/AAtest.c
+	
+	// Terminal command 
+	//  gcc -o AAtest /home/maurizio/Desktop/Cs50x_practice_21/AAtest.c -I./libcs50/src -L./libcs50 -lcs50
+}
