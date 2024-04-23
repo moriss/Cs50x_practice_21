8bb061484970c165549c57526cca5e52ec78e258
moriss <marrocco001@gmail.com>
2024-04-23 09:24:32 +0100

    all

--- AAtest.c
+++ AAtest.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <cs50.h>
+//#include <cs50.h>
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
@@ -7,19 +7,18 @@
 // Ask someone their name and print it out using a function.
 //ALLTHEWAZE
 // 1.In main  2.As a function  3.no input/outpuT  #4.input  5.output  6.input/output 7. Save output as a variable
-void my()
+
+	int main(int argc, char* argv[])
 {
-	char name[  7];
-	printf(" What is your name ? ");
-	scanf("%6s",name);
-	printf("Hello %s\n  ",name  );
+	if (argc ==3)
+	{
+	int a = atoi(argv[1]) + atoi(argv[2]);
+		printf("%i " , a);
+	}
+	else
+	printf("You have no additional inputs after ./filename \n");
 }
 
-int main() 
-{
-	char * x = my();
-	return x;
-}
 /*
 void my() {
     char name[100]; // Assuming a maximum input length of 99 characters
@@ -41,6 +40,7 @@ int main() {
 	// /home/maurizio/Desktop/Cs50x_practice_21/AAtest.c
 	
 	// Terminal command 
-	//  gcc -o AAtest /home/maurizio/Desktop/Cs50x_practice_21/AAtest.c -I../libcs50/src -L../libcs50 -lcs50
+	//  gcc -o AAtest /home/maurizio/Desktop/Cs50x_practice_21/AAtest.c -I../libcs50/src -L../libcs50 -lcs50ls
+	
 	
 	//Then run ./AAtest
